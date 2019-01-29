5554e25a9b6f2da3d66c1be57523f025fa232b48
Alexandre Quemy <alexandre.quemy@gmail.com>
2019-01-29 16:36:50 +0100

    Correct the class name

--- multiclass_confusion_matrices.py
+++ multiclass_confusion_matrices.py
@@ -3,8 +3,9 @@ from os import path
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
+import sys
 from matplotlib.colors import ListedColormap
-from echr_experiments.config import ROUND_DIGITS, ANALYSIS_PATH, MULTICLASS_OUTPUT_FILE
+from echr_experiments.config import ROUND_DIGITS, ANALYSIS_PATH, MULTICLASS_OUTPUT_FILE, INPUT_PATH
 from echr_experiments.format import floatify
 from echr_experiments.plot import make_confusion_matrix_plot
 
@@ -12,10 +13,22 @@ from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 
 output_file = MULTICLASS_OUTPUT_FILE
+outcome_file = path.join(INPUT_PATH, 'multiclass', 'outcomes_variables.json')
+
+def sort_article(a):
+    i = a[0].split(' ')[1]
+    if i == 'p1':
+        return sys.maxint
+    return int(i)
 
 def main():
     with open(output_file) as f:
         raw_results = json.load(f)
+
+    with open(outcome_file) as f:
+        outcomes_data = json.load(f)
+
+    outcomes_mapping = {str(v):k for k,v in outcomes_data.iteritems()}
 
     #dataset = "Multiclass - Descriptive features only"
     for dataset in raw_results.keys():
@@ -25,7 +38,7 @@ def main():
                 #method = "Random Forest"
                 cnf_matrix = data["confusion_matrix"]["{}_n".format(target)]
                 class_names = data["confusion_matrix"]['class_labels']
-                class_names = ['Art. {} - {}'.format(n.split(':')[0], 'V' if n.split(':')[1] == '1' else 'NV') for n in class_names]
+                class_names = ['Art. {} - {}'.format(outcomes_mapping[n.split(':')[0]], 'V' if n.split(':')[1] == '1' else 'NV') for n in class_names]
                 cnf_matrix = floatify(cnf_matrix)
                 cnf_matrix = np .array([np.array(xi) for xi in cnf_matrix])
 
